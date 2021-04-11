fa83ad7c837839139ceda9e5cab73d280d440433
Jade-ux <jadeemailsandbox@gmail.com>
2021-04-11 10:49:11 +0000

    Installed Pillow. Created models for category, artist, country and artworks. Loaded all data from fixtures. Registered the models in admin.

--- artworks/admin.py
+++ artworks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Country, Artist, Artwork
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Country)
+admin.site.register(Artist)
+admin.site.register(Artwork)
--- artworks/models.py
+++ artworks/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    country = models.ForeignKey(
+        'Country', null=True, blank=True, on_delete=models.SET_NULL)
+    biography = models.TextField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Artwork(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    country = models.ForeignKey(
+        'Country', null=True, blank=True, on_delete=models.SET_NULL)
+    artist = models.ForeignKey(
+        'Artist', null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField()
+    image = models.ImageField()
+    original_medium = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    size = models.CharField(max_length=254)
+    featured = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
