8f0dd685135c24c7e6ad776c45c8fc1b14b809ae
Jade-ux <jadeemailsandbox@gmail.com>
2021-04-11 22:29:25 +0000

    Added search functionality to all artworks view.

--- artworks/views.py
+++ artworks/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import (
+    render, get_object_or_404, redirect, reverse)
+from django.contrib import messages
+from django.db.models import Q
 from .models import Artwork
 
-# Create your views here.
 
 def all_artworks(request):
     """ A view to show all artworks, including sorting and search queries """
 
     artworks = Artwork.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "Please enter a search criteria")
+                return redirect(reverse('artworks'))
+
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            artworks = artworks.filter(queries)
 
     context = {
         'artworks': artworks,
+        'search_term': query,
     }
 
     return render(request, 'artworks/artworks.html', context)
