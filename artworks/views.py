59e238e52811dc3bcae7deab700c99c519f3905f
Jade-ux <jadeemailsandbox@gmail.com>
2021-04-11 11:29:44 +0000

    Added artworks views and template and registered the URL.

--- artworks/views.py
+++ artworks/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Artwork
 
 # Create your views here.
+
+def all_artworks(request):
+    """ A view to show all artworks, including sorting and search queries """
+
+    artworks = Artwork.objects.all()
+
+    context = {
+        'artworks': artworks,
+    }
+
+    return render(request, 'artworks/artworks.html', context)
