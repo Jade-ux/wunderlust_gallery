54c336b6c3b5da507d820ad7a321a9ddcf3e8879
Jade-ux <jadeemailsandbox@gmail.com>
2021-04-11 16:26:26 +0000

    Added the artwork detail page.

--- artworks/urls.py
+++ artworks/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_artworks, name='artworks'),
+    path('<artwork_id>', views.artwork_detail, name='artwork_detail'),
 ]
--- artworks/views.py
+++ artworks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Artwork
 
 # Create your views here.
@@ -13,3 +13,15 @@ def all_artworks(request):
     }
 
     return render(request, 'artworks/artworks.html', context)
+
+
+def artwork_detail(request, artwork_id):
+    """ A view to show artwork details on a new page """
+
+    artwork = get_object_or_404(Artwork, pk=artwork_id)
+
+    context = {
+        'artwork': artwork,
+    }
+
+    return render(request, 'artworks/artwork_detail.html', context)
