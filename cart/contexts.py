34cd9f42dc2474f5b7a6e32f354ddd5d6407d4c0
Jade-ux <jadeemailsandbox@gmail.com>
2021-04-13 13:29:37 +0000

    Removed frame options.

--- cart/contexts.py
+++ cart/contexts.py
@@ -10,31 +10,16 @@ def cart_contents(request):
     total = 0
     artwork_count = 0
     cart = request.session.get('cart', {})
-    frame_price = 0
 
-    for item_id, item_data in cart.items():
-        if isinstance(item_data, int):
-            artwork = get_object_or_404(Artwork, pk=item_id)
-            frame_price = artwork.price * Decimal(settings.FRAME_PERCENTAGE)
-            total += item_data * artwork.price + frame_price
-            artwork_count += item_data
-            cart_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'artwork': artwork,
-                'frame_price': frame_price,
-            })
-        else:
-            artwork = get_object_or_404(Artwork, pk=item_id)
-            for frame, item_data in item_data['items_by_frame'].items():
-                total += item_data * artwork.price + frame_price
-                artwork_count += item_data
-                cart_items.append({
-                    'item_id': item_id,
-                    'quantity': item_data,
-                    'artwork': artwork,
-                    'frame_price': frame_price,
-                })
+    for item_id, quantity in cart.items():
+        artwork = get_object_or_404(Artwork, pk=item_id)
+        total += quantity * artwork.price
+        artwork_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'artwork': artwork,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -42,18 +27,16 @@ def cart_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'cart_items': cart_items,
         'total': total,
         'artwork_count': artwork_count,
-        'frame_price': frame_price,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        # 'frame_price': settings.FRAME_PERCENTAGE,
         'grand_total': grand_total,
     }
 
--- cart/views.py
+++ cart/views.py
@@ -1,38 +1,24 @@
-  
 from django.shortcuts import render, redirect
 
-# Create your views here.
 
 def view_cart(request):
     """ A view that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
 
+
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified artwork to the shopping cart """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    frame_option = None
-    if 'artwork_frame' in request.POST:
-        frame = request.POST['artwork_frame']
     cart = request.session.get('cart', {})
 
-    # items_by_frame dictionary 
-    # allows tracking of multiple sizes for single items
-    if frame:
-        if item_id in list(cart.keys()):
-            if frame in cart[item_id]['items_by_frame'].keys():
-                cart[item_id]['items_by_frame'][frame] += quantity
-            else:
-                cart[item_id]['items_by_frame'][frame] = quantity
-        else:
-            cart[item_id] = {'items_by_frame': {frame: quantity}}
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
     else:
-        if item_id in list(cart.keys()):
-            cart[item_id] += quantity
-        else:
-            cart[item_id] = quantity
+        cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+    
