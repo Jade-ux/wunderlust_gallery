56b322094c51873020878b1af16b7860b1c352d8
Jade-ux <jadeemailsandbox@gmail.com>
2021-04-13 11:13:12 +0000

    Added frame options.

--- cart/views.py
+++ cart/views.py
@@ -13,12 +13,26 @@ def add_to_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    frame_option = None
+    if 'artwork_frame' in request.POST:
+        frame = request.POST['artwork_frame']
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    # items_by_frame dictionary 
+    # allows tracking of multiple sizes for single items
+    if frame:
+        if item_id in list(cart.keys()):
+            if frame in cart[item_id]['items_by_frame'].keys():
+                cart[item_id]['items_by_frame'][frame] += quantity
+            else:
+                cart[item_id]['items_by_frame'][frame] = quantity
+        else:
+            cart[item_id] = {'items_by_frame': {frame: quantity}}
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(redirect_url)
