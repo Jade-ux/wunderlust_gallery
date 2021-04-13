c7588d733bffd8618e11d73cde8af1eda658f047
Jade-ux <jadeemailsandbox@gmail.com>
2021-04-13 09:07:33 +0000

    Added items to the shopping cart page and styled the page.

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from artworks.models import Artwork
 
 
 def cart_contents(request):
@@ -7,6 +9,17 @@ def cart_contents(request):
     cart_items = []
     total = 0
     artwork_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        artwork = get_object_or_404(Artwork, pk=item_id)
+        total += quantity * artwork.price
+        artwork_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'artwork': artwork,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -24,6 +37,7 @@ def cart_contents(request):
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        # 'frame_price': settings.FRAME_WHITE_PERCENTAGE,
         'grand_total': grand_total,
     }
 
