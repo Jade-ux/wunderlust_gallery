5f767e7bffe88efac66e21afc3144803b9a58ffa
Jade-ux <jadeemailsandbox@gmail.com>
2021-04-26 08:50:21 +0000

    Added order summary to the checkout success page.

--- checkout/views.py
+++ checkout/views.py
@@ -32,11 +32,7 @@ def checkout(request):
 
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_cart = json.dumps(cart)
-            order.save()
+            order = order_form.save()
             for item_id, quantity in cart.items():
                 try:
                     artwork = Artwork.objects.get(id=item_id)
