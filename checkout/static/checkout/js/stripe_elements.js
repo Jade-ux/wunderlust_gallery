1a11c2c9d5f51c50ec3cdebe7be86dedc9d9432c
Jade-ux <jadeemailsandbox@gmail.com>
2021-04-27 13:20:47 +0000

    Added user profile to webhook handler.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -111,7 +111,7 @@ form.addEventListener('submit', function(ev) {
             $('#submit-btn').attr('disabled', false);
         } else {
             if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+               form.submit();
             }
         }
     });
