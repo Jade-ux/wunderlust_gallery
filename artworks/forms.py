5113c41ce42328696fe5e2a6447961d14e6a128f
Jade-ux <jadeemailsandbox@gmail.com>
2021-04-27 14:48:28 +0000

    Added artist friendly name to the form.

--- artworks/forms.py
+++ artworks/forms.py
@@ -1,6 +1,6 @@
 
 from django import forms
-from .models import Artwork, Category
+from .models import Artwork, Category, Artist
 
 
 class ArtworkForm(forms.ModelForm):
@@ -14,7 +14,10 @@ class ArtworkForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        artists = Artist.objects.all()
+        friendly_name_artist = [(a.id, a.get_friendly_name()) for a in artists]
 
         self.fields['category'].choices = friendly_names
+        self.fields['artist'].choices = friendly_name_artist
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-grey'
